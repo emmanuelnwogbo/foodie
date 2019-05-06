332df095bad93c8e244c176056569021b6cf86b2
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-05-06 07:03:49 +0100

    added search bar and search input ajax request with axios

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,38 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import './scss/main.scss';
+import { key, proxy } from '../config';
+
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: key,
+      query: 'eggs',
+    }
+  }
+
+  componentDidMount() {
+    //this.getResults().then()
+  }
+
+  getResults = (query)  => {
+    axios.get(`${proxy}http://food2fork.com/api/search?key=${this.state.key}&q=${query}`)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   render() {
     return (
-      <div>
-      <p>hello world</p>
+      <div className="app">
+        <SearchBar getResults={this.getResults}/>
+        <p>hello world</p>
       </div>
     )
   }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+import '../scss/components/searchBar.scss';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: null
+    }
+  }
+
+  componentDidMount() {
+    console.log(this.props)
+  }
+
+  getQuery = (event) => {
+    let query;
+    if (event.key === 'Enter') {
+      query = event.target.value;
+      this.setState({ query }, () => {
+        console.log('search: ', this.state)
+        this.props.getResults(this.state.query)
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="searchBar">
+        <input type="text" onKeyPress={this.getQuery} className="searchBar--input" placeholder="find recipes"/>
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
