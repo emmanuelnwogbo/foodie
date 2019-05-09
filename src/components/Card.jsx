e58c27d55995375cdf813c0aa34cbe296e16f9d4
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-05-09 01:44:05 +0100

    877499038787498575478

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,40 +3,6 @@ import React, { Component } from 'react';
 class Card extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      img: null
-    }
-  }
-
-  lazyLoadImg = (recipe) => {
-    const img = new Image();
-    img.src = this.props.recipe.image_url;
-    img.onload = () => {
-      this.setState({ img })
-    }
-
-    if (this.state.img !== null) {
-      return (
-        <div className="container__card--parent">
-          <div className="container--card">
-            <figure className="container--figure">
-              <img className="container--img" src={`${this.state.img.src}`}/>
-            </figure>
-            <div className="container--details">
-              <p className="container--title">{recipe.title}</p>
-              <p className="container--publisher" onClick={this.openPublisher}>{recipe.publisher}</p>
-            </div>
-            <div className="container__viewmore">
-              <span className="container__viewmore--btn" onClick={this.openRecipe}><p>view</p></span>
-            </div>
-          </div>
-        </div>
-      )
-    }
-
-    return (
-      <div className="lazyLoading--placeholder container__card--parent"></div>
-    )
   }
 
   openRecipe = () => {
@@ -55,7 +21,22 @@ class Card extends Component {
 
   render() {
     const { recipe } = this.props;
-    return this.lazyLoadImg(recipe)
+    return (
+      <div className="container__card--parent">
+        <div className="container--card">
+          <figure className="container--figure">
+            <img className="container--img" src={recipe.image_url}/>
+          </figure>
+          <div className="container--details">
+            <p className="container--title">{recipe.title}</p>
+            <p className="container--publisher" onClick={this.openPublisher}>{recipe.publisher}</p>
+          </div>
+          <div className="container__viewmore">
+            <span className="container__viewmore--btn" onClick={this.openRecipe}><p>view</p></span>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
